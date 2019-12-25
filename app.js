648a2554137794cec2129579f3396ce089ee4350
Natalie Ng <thuycnn91@gmail.com>
2019-12-25 18:08:47 -0500

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var express = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
 var app = express();
 var port = 3000;
 const path = require('path');
- 
+
+const bodyParser = require('body-parser');// body parser :To parse the data in the body 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;// mongoose
+mongoose.connect("mongodb://localhost:27017/quotes");
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "\\views\\index.html")); // this is what happen when you click quotes.com/
+    
 });
 
 app.get("/about", (req,res) => {
@@ -12,10 +21,14 @@ app.get("/about", (req,res) => {
 });
 
 app.post("/submit-form", (req,res) => {
-    const username = req.body;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const phoneNumber = req.body.phoneNumber;
+
     console.log(username);
 });
  
+// add the router
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
