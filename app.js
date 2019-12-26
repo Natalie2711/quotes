91d7bb2e33b5cc2b3b7d8a13f8384e8dca887ef4
Natalie Ng <thuycnn91@gmail.com>
2019-12-26 18:49:40 -0500

    third commit

--- app.js
+++ app.js
@@ -8,12 +8,29 @@ const bodyParser = require('body-parser');// body parser :To parse the data in t
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.Promise = global.Promise;// mongoose
-mongoose.connect("mongodb://localhost:27017/quotes");
+// view engine setup
+app.set("views", "views"); // set view property to the path of view directory
+app.set("view engine", "pug"); // update the ‘view engine’ property  set its value to pug
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "\\views\\index.html")); // this is what happen when you click quotes.com/
-    
+// connect to the database
+mongoose.Promise = global.Promise;// mongoose
+mongoose.connect("mongodb://localhost:27017/quotes_db");
+
+// create database schema
+const qpackageSchema = new mongoose.Schema({ // define attributes for documents saved and retrieved from database   
+    title: String,
+    quotes: Array
+});
+
+// create a model qpackage to use 
+const qpackagesModel = mongoose.model("qpackages", qpackageSchema) // use this model to read from database and
+
+
+app.get('/', async function (req, res) {
+    // get all package titles
+    const titles = await qpackagesModel.find({}, "title");
+    console.log(req.body);
+    res.render('index', {titles: titles.map(v => v.title)});
 });
 
 app.get("/about", (req,res) => {
@@ -21,15 +38,15 @@ app.get("/about", (req,res) => {
 });
 
 app.post("/submit-form", (req,res) => {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
+    const name = req.body.name;
     const phoneNumber = req.body.phoneNumber;
-
-    console.log(username);
+    const qpackage = req.body.title;
+    
 });
  
 // add the router
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
+
 
