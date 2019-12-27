f9a3494e025c9879750ee5bf8f2e81f18b04597e
Natalie Ng <thuycnn91@gmail.com>
2019-12-26 19:49:38 -0500

    fourth commit

--- app.js
+++ app.js
@@ -19,29 +19,37 @@ mongoose.connect("mongodb://localhost:27017/quotes_db");
 // create database schema
 const qpackageSchema = new mongoose.Schema({ // define attributes for documents saved and retrieved from database   
     title: String,
-    quotes: Array
+    quotes: Array,
+    _id: Number
 });
 
-// create a model qpackage to use 
+// create a package model qpackage to use 
 const qpackagesModel = mongoose.model("qpackages", qpackageSchema) // use this model to read from database and
 
+// create database schema
+const userSchema = new mongoose.Schema({ // define attributes for documents saved and retrieved from database   
+    name: String,
+    phoneNumber: Number,
+    packageId: Number
+});
+
+// create a user model qpackage to use 
+const userModel = mongoose.model("users", userSchema) // use this model to read from database and
 
 app.get('/', async function (req, res) {
     // get all package titles
-    const titles = await qpackagesModel.find({}, "title");
-    console.log(req.body);
-    res.render('index', {titles: titles.map(v => v.title)});
+    const packages = await qpackagesModel.find({}, "_id title");
+    console.log(packages);
+    res.render('index', {packages: packages});
 });
 
-app.get("/about", (req,res) => {
+app.get("/about", (req, res) => {
     res.send("Introduction about the project") // send string, later can send html file
 });
 
-app.post("/submit-form", (req,res) => {
-    const name = req.body.name;
-    const phoneNumber = req.body.phoneNumber;
-    const qpackage = req.body.title;
-    
+app.post("/submit-form", (req, res) => {
+    const user = new userModel(req.body);
+    user.save().then(item => res.send("saved to database.")); // data from the submit form , written to the database
 });
  
 // add the router
